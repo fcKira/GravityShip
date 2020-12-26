e2f55418776bf77cc20e0c101365ae7a00aee434
Frankie <francow.correia@gmail.com>
2020-12-26 05:50:48 -0300

    Portal meti2 y scripteado

--- Assets/Scripts/Attractors/EndPortal.cs
+++ Assets/Scripts/Attractors/EndPortal.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EndPortal : MonoBehaviour
+public class EndPortal : AttractorObj
 {
     void Win()
     {
-
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
